e844c06521c36025d44b6f93ec154b0b03af6fa7
‘3sunshine3’ <2676405606@qq.com>
2019-03-20 20:42:12 +0800

    update userlist

--- admin/user_list.php
+++ admin/user_list.php
@@ -61,6 +61,51 @@ require("../oj-footer.php");
     <div style="width: 80%;padding-top: 20px">
         <!--以下为两个必须div元素-->
         <div id="table"></div>
+<div>
+        <?php
+        echo "555555555555555555555555555555555555556666666666666666666666666666666666666666666666";
+        echo "<title>User List</title>";
+        echo "<center><h2>User List</h2></center>";
+        require_once("../include/set_get_key.php");
+        $sql="";
+        if(isset($_GET['keyword'])){
+            $keyword=$_GET['keyword'];
+            $keyword="%$keyword%";
+            $sql="select `user_id`,`nick`,`reg_time`,`ip`,`school`,`defunct` FROM `users` where user_id like ? or nick like ? ";
+            $result=pdo_query($sql,$keyword);
+        }else{
+            $sql="select `user_id`,`nick`,`reg_time`,`ip`,`school`,`defunct` FROM `users`  order by `user_id` ASC;";
+            $result=pdo_query($sql);
+        }
+        ?>
+
+
+        <?php
+        echo "<center><table class='table table-striped' width=90% border=1>";
+        echo "<tr><td>UserID<td>Nick<td>Status<td>RegTime<td>IP<td>School";
+        echo "</tr>";
+        foreach($result as $row){
+            echo "<tr>";
+            echo "<td><a href='../userinfo.php?user=".$row['user_id']."'>".$row['user_id']."</a>";
+            echo "<td>".$row['nick'];
+            $cid=$row['user_id'];
+            if(isset($_SESSION[$OJ_NAME.'_'.'administrator'])){
+                echo "<td><a href=user_df_change.php?cid=".$row['user_id']."&getkey=".$_SESSION[$OJ_NAME.'_'.'getkey'].">".($row['defunct']=="N"?"<span class=green>Available</span>":"<span class=red>Reserved</span>")."</a>";
+            }else{
+                echo "<td>No permissions";
+            }
+            echo "<td>".$row['reg_time'];
+            echo "<td>".$row['ip'];
+            echo "<td>".$row['school'];
+            echo "</tr>";
+        }
+        echo "</table></center>";
+        require("../oj-footer.php");
+        ?>
+
+</div>
+
+
         <div id="pageBar"></div>
     </div>
 
@@ -75,12 +120,12 @@ require("../oj-footer.php");
     //标准json格式 目前只支持[{a:b,c:d},{a:b,c:d}]此种格式
 
         var json = [{
-            "UserID": "<a>哈哈哈</a>",
-            "Nick": "Tom",
-            "Status": "12",
-            "RegTime": "12",
-            "IP": "12",
-            "School": "12"
+            "UserID":"1" ,
+            "Nick": "1",
+            "Status": "1",
+            "RegTime": "1",
+            "IP": "1",
+            "School": "1"
         }/*, {
             "UserID": "2",
             "Nick": "Tom",
@@ -193,28 +238,7 @@ require("../oj-footer.php");
             "RegTime": "12",
             "IP": "12",
             "School": "12"
-        }, {
-            "UserID": "18",
-            "Nick": "Tom",
-            "Status": "12",
-            "RegTime": "12",
-            "IP": "12",
-            "School": "12"
-        }, {
-            "UserID": "19",
-            "Nick": "Tom",
-            "Status": "12",
-            "RegTime": "12",
-            "IP": "12",
-            "School": "12",
-        }, {
-            "UserID": "20",
-            "Nick": "Tom",
-            "Status": "12",
-            "RegTime": "12",
-            "IP": "12",
-            "School": "12"
-        }, {
+        },  {
             "UserID": "21",
             "Nick": "Tom",
             "Status": "12",
