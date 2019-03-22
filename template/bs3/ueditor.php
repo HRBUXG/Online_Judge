172ac2f059f955c311295a2aee2c7640c2706118
DragonLee <840877228@qq.com>
2019-03-22 10:13:37 +0800

    UEditor依赖文件

---
+++ template/bs3/ueditor.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+<title>Document</title>
+<script type="text/javascript" charset="utf-8" src="/Develop/ueditor/ueditor.config.js"></script>
+<script type="text/javascript" charset="utf-8" src="/Develop/ueditor/ueditor.all.js"></script>
+<script type="text/javascript" charset="utf-8" src="/Develop/ueditor/lang/zh-cn/zh-cn.js"></script>
+<script>
+    UE.getEditor('UEditor_Description',{initialFrameWidth:0,initialFrameHeight:0});
+    var ue_description = UE.getEditor('UEditor_Description');
+    UE.getEditor('UEditor_Input',{initialFrameWidth:0,initialFrameHeight:0});
+    var ue_input = UE.getEditor('UEditor_Input');
+    UE.getEditor('UEditor_Output',{initialFrameWidth:0,initialFrameHeight:0});
+    var ue_output = UE.getEditor('UEditor_Output');
+    UE.getEditor('UEditor_Hint',{initialFrameWidth:0,initialFrameHeight:0});
+    var ue_hint = UE.getEditor('UEditor_Hint');
+
+</script>
