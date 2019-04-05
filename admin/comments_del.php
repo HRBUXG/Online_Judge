e8c51d04723274895bea3d74ecc5b10fe02217ac
wwj <2388545772@qq.com>
2019-04-05 20:57:50 +0800

    管理员现在可以正确的管理言论不当评论（其实就是后台删除评论）

--- admin/comments_del.php
+++ admin/comments_del.php
@@ -9,8 +9,7 @@ $conn = mysqli_connect("localhost", "root", "HRBUXGOJ");
 mysqli_query($conn, "set names utf8");
 if (!$conn) {
     echo "连接失败";
-}
-else{
+} else {
     $id = $_GET['id'];
     mysqli_select_db($conn, "jol");
     $sql = "delete from comment where id =" . $id;
@@ -18,5 +17,5 @@ else{
 }
 
 mysqli_close($conn);
-
+echo "删除成功！";
 ?>
